af4bc48d9589af9e8d5b63892411da13c1b26726
adsiitam12 <mike.glz.96@gmail.com>
2018-03-23 09:59:52 -0600

    First Commit

---
+++ src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
@@ -0,0 +1,16 @@
+package mx.com.itam.adsi.ejercicio;
+public class Principal{
+	
+	public static void main(String ... argv){
+		System.out.println(convert("FFF1","0123456789ABCDEF"))
+	}
+
+	private static int convert(String source, String alphabet){
+		int res = 0;
+		int base = alphabet.length();
+		for(char c : source.toCharArray()){
+			res = res*base + alphabet.indexOf(c);
+		}
+		return res;
+	}
+}
